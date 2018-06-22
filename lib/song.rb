eca3e52767f427344e80a561e406e6f2b0c44de5
mcauliffek4298 <kathryn@peerlift.org>
2018-06-22 20:21:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,31 @@
+class Song
+  
+  attr_accessor :genre, :artist, :name  #to replaces our gets_genre, gets_artist, and gets_name method(s)
 
+@@count = 0
+  
+def initialize(genre, artist, name)
+  @genre=genre
+  @artist=artist
+  @name=name
+  @@count += 1
+end
+
+
+def get_count
+  @@count
+end
+  
+end
+
+jon_song = Song.new("Hippity Hoppity", "Jon Mendez", "Jon's Way")
+
+puts jon_song.get_count
+
+
+
+
+# puts jon_song.genre
+# puts "wow! what a great choice!"
+# puts jon_song.name
+# puts jon_song.artist
